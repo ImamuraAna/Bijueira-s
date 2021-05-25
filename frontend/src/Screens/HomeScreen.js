4bdb691206fb5799697e802243d4eb0350be16f8
ImamuraAna <anacarolineimamura@gmail.com>
2021-05-25 00:52:32 -0300

    create server

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
 
 function HomeScreen (props){
+
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios.defaults.port = 5000;
+    const fetchData = async () =>{
+      const {data} = await axios.get('http://192.168.15.164:5000/api/products');
+      setProduct(data);
+    }
+    fetchData();
+    return () => {
+      //
+    };
+  }, [])
+
     return <ul class="products"> 
                   {
-                    data.products.map(product =>
+                    products.map(product =>
                       <li>
                         <div class="product">
                             <img class="product-img" src={product.image} alt="product"></img>
